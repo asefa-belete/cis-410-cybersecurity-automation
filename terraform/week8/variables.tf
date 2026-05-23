12f165b988b2829ba0817e1b190e8cd8dd0afe72
Asefa Belete <asefabelete@users.noreply.github.com>
2026-05-23 15:47:51 -0700

    Week 8: Cloud Run + Artifact Registry + comparison docs

---
+++ terraform/week8/variables.tf
@@ -0,0 +1,33 @@
+
+# terraform/week8/variables.tf
+
+
+variable "project_id" {
+
+  description = "cis410-asefa"
+
+  type        = string
+
+}
+
+
+variable "region" {
+
+  description = "GCP region"
+
+  type        = string
+
+  default     = "us-central1"
+
+}
+
+
+variable "container_image" {
+
+  description = "Full Artifact Registry image path including commit SHA tag"
+
+  type        = string
+
+  # Format: us-central1-docker.pkg.dev/PROJECT_ID/cis410-app/flask-app:COMMIT_SHA
+
+}
